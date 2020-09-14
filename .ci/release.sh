fc4eaede46259a28068e7102300928572e5f1a48
Abhinay Agarwal <abhinay.agarwal@gluonhq.com>
2020-09-14 17:30:32 +0530

    Release to Maven Central

--- .ci/release.sh
+++ .ci/release.sh
@@ -1,11 +1,21 @@
 #!/usr/bin/env bash
 
+# Exit immediately if any command in the script fails
+set -e
+
 # Configure GIT
 git config --global user.email "githubbot@gluonhq.com"
 git config --global user.name "Gluon Bot"
 
+# Decrypt encrypted files
+openssl aes-256-cbc -K $encrypted_dc87922a4c8c_key -iv $encrypted_dc87922a4c8c_iv -in .ci/secring.gpg.enc -out secring.gpg -d
+if [[ ! -s secring.gpg ]]
+   then echo "Decryption failed."
+   exit 1
+fi
+
 # Generate and upload jars
-./gradlew publish -PrepositoryUrl=$mavenPublishURL/releases/ -PrepositoryUsername=$mavenPublishUsername -PrepositoryPassword=$mavenPublishPassword
+./gradlew publish closeAndReleaseRepository -i -PsonatypeUsername=$SONATYPE_USERNAME -PsonatypePassword=$SONATYPE_PASSWORD -Psigning.keyId=$GPG_KEYNAME -Psigning.password=$GPG_PASSPHRASE -Psigning.secretKeyRingFile=$TRAVIS_BUILD_DIR/secring.gpg
 
 # Upload to S3
 touch ~/.s3cfg
@@ -18,6 +28,8 @@ sed -i "s,url=.*\",url=/connect/javadoc/$TRAVIS_TAG\",g" index.html
 # Update index.html to latest release version
 s3cmd --no-mime-magic --guess-mime-type --access_key "$AWS_ACCESS_KEY" --secret_key "$AWS_SECRET_KEY" put -P index.html s3://docs.gluonhq.com/connect/javadoc/
 
+# Update version by 1
+newVersion=${TRAVIS_TAG%.*}.$((${TRAVIS_TAG##*.} + 1))
 # Replace first occurrence of
 # version = 'TRAVIS_TAG' 
 # with 
